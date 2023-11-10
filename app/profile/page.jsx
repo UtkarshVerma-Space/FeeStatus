59bee226bb5bd68209d6fe4af9ba0ffa95489fb1
UtkarshVerma-Coder <utkarshverma001@outlook.com>
2023-11-11 01:58:33 +0530

    Declared var

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -8,24 +8,26 @@ export default function Profile() {
   const auth = getAuth();
   const user = auth.currentUser;
 
-  let displayName, email; 
-
+  // Declaring displayname and email here so that if firbase do not loads the default value will be none and app will run normal
+    let displayName,email;
 
   if (user !== null) {
     // The user object has basic properties such as display name, email, etc.
-    const email = user.email;
-    const photoURL = user.photoURL;
-    const emailVerified = user.emailVerified;
-    const uid = user.uid;
+    // const email = user.email;
+    // const photoURL = user.photoURL;
+    // const emailVerified = user.emailVerified;
+    // const uid = user.uid;
+    displayName = user.displayName;
+    email = user.email;
+
   }
   return (
     <>
       <div className={style.container}>
         <div className={style.profilebox}>
           <div className={style.info}>
-            <div className={style.name}>{user.displayName}</div>
-            <div className={style.class}>{user.email}</div>
-            <div className={style.phn}>{user.displayName}</div>
+            <div className={style.name}>{displayName}</div>
+            <div className={style.class}>{email}</div>
           </div>
 
           <div className={style.features}>
